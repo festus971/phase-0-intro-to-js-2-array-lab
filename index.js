9916e09363c8afaef3c86037291c355f7c5440fa
+ <+>
2022-06-08 11:40:23 +0300

    commit tree e94d0b93e5f89c7cfa87199d4c895b4dc0f91b61

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ console.log(cats);
 function destructivelyAppendCat(name) {
   cats.push(name);
 }
-function destructivelyPrependCat(name){
-cats.unshift(name)}
-function destructivelyRemoveFirstCat(){
-    cats.shift();}
-function destructivelyRemoveLastCat(){
-    cats.pop();
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
 }
-function appendCat(name){
-return[...cats,name]}
-function prependCat(name){
-    return[name,...cats];
+function destructivelyRemoveFirstCat() {
+  cats.shift();
 }
-function removeFirstCat(){
-    return cats.slice(1);
+function destructivelyRemoveLastCat() {
+  cats.pop();
 }
-function removeLastCat(){
-    return cats.slice(0,cats.length-1);
+function appendCat(name) {
+  return [...cats, name];
+}
+function prependCat(name) {
+  return [name, ...cats];
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
 }
